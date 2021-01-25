cdccc9481b9ae7d279a78feb9c7487572e391446
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-26 00:36:01 +0300

    added generating colors on ColorScreen

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,10 +1,32 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button } from "react-native";
+import { StyleSheet, FlatList, View } from "react-native";
+
+const randomNumber = () => Math.floor(Math.random() * (255 - 0 + 1)) + 0;
 
 const ColorScreen = () => {
+  const [colors, setColor] = useState([]);
+  const color = (
+    <View
+      style={{
+        height: 100,
+        width: 100,
+        backgroundColor: `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
+      }}
+    />
+  );
   return (
     <View>
-      <Text>ColorScreen ColorScreen</Text>
+      <Button
+        title="Add a Color"
+        onPress={() => setColor([color, ...colors])}
+      />
+      <FlatList
+        data={colors}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => item}
+        horizontal
+      />
     </View>
   );
 };
