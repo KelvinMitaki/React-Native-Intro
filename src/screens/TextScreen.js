d94b1efc8db15c417033e2ff43a5f2f094ff2eb0
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-26 12:58:38 +0300

    added functionality to text inp

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const TextScreen = () => {
+  const [input, setInput] = useState("");
   return (
     <View>
+      <Text>ENTER NAME:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        onChangeText={text => setInput(text)}
+        value={input}
       />
+      <Text>My name is {input}</Text>
     </View>
   );
 };
