da7296146c97274d03801c4dda408946ab3fb587
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-26 12:20:08 +0300

    reformatted CounterScreen to use usereducer instead of usestate

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increase_counter":
+      return { ...state, counter: state.counter + action.payload };
+    case "decrease_counter":
+      return { ...state, counter: state.counter - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
   return (
     <View>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => setCount(c => c + 1)}
+        onPress={() => dispatch({ type: "increase_counter", payload: 1 })}
       >
         <Text>Increase</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => setCount(c => c - 1)}
+        onPress={() => dispatch({ type: "decrease_counter", payload: 1 })}
       >
         <Text>Decrease</Text>
       </TouchableOpacity>
       <Text>Current Count:</Text>
-      <Text>{count}</Text>
+      <Text>{state.counter}</Text>
     </View>
   );
 };
